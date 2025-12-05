b6ff39004497fb3f827f217455c1abd608b325d5
ferorVD <derorferor@gmail.com>
2025-12-05 21:11:45 +0300

    первый коммит

---
+++ app.py
@@ -0,0 +1,6 @@
+def greet(name: str):
+    return f"Hi! {name}"
+def add(a:int, b:int):
+    return a+b
+def mul(a: int, b: int):
+    return a * b                
---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+from app import greet, add, mul
+
+def test_greet():
+    assert greet("Bob") == "Hi! Bob" 
+
+def test_add():
+    assert add (2, 3) == 5
+
+def test_mul():
+    assert mul(2, 5) == 10         
